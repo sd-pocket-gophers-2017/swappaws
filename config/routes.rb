1d33797ce1a65f27b3b7a12b327225092e108da3
Blake Meyer <blake.ynwa@gmail.com>
2017-09-07 15:15:52 -0700

    Moved app root location

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'tests#index' 
   devise_for :users
   resources :tests
-  root 'tests#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
