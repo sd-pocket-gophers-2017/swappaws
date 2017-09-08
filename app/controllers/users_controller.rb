826c66cc9e621e440ecddd72a5b5e653b83ebc87
Blake Meyer <blake.ynwa@gmail.com>
2017-09-08 11:43:56 -0700

    Renamed all erb files to include .html.erb at the end, started work on user profile show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def show
-    current_user
+    if current_user
+      current_user
+    else
+      redirect_to '/users/sign_in'
+    end
   end
 
 end
