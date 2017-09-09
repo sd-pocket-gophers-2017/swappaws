86aff2470476270dcb500f4f32cf48131fcd467e
Carter McGee <carterm912@gmail.com>
2017-09-09 12:58:39 -0700

    Update....

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   has_many :pets
   has_many :participants
   has_many :events, through: :participants
+  has_many :roles, through: :participants
   validates :first_name, :last_name, presence: true
 end
