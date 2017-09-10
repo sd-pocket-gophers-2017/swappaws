ad43c397b1b8a1b4f3b569a0cc0e1a98fe6087c2
Mark Kim <mark.jn.kim@gmail.com>
2017-09-09 19:31:18 -0700

    fixed bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
 
-  belongs_to :user
+  belongs_to :owner, class_name: :User
+  belongs_to :sitter, class_name: :User
   has_many :participants
   has_many :pets, through: :user
 
