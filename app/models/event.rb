fc60fbc68a29008daaee5a778e2b9e6004ac074d
Carter McGee <carterm912@gmail.com>
2017-09-10 12:10:26 -0700

    add optional is true to event files

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  belongs_to :sitter, class_name: :User
-  belongs_to :owner, class_name: :User
+  belongs_to :sitter, class_name: :User, optional: true
+  belongs_to :owner, class_name: :User, optional: true
 end
