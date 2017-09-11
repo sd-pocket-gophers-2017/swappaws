9feb78c4f5de588f8b235655c36f2a464ccbaae8
Blake Meyer <blake.ynwa@gmail.com>
2017-09-10 19:22:20 -0700

    Add token attribute to users. Display tokens on the users profile page. Default token value is 3, going to work on incrementing/decrementing token count now

--- db/migrate/20170907222832_add_attributes_to_users.rb
+++ db/migrate/20170907222832_add_attributes_to_users.rb
@@ -2,5 +2,6 @@ class AddAttributesToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    add_column :users, :tokens, :integer, default: 3
   end
 end
