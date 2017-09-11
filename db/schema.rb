548a827071f32cd19e69f9034665f66973c0ea7e
Mark Kim <mark.jn.kim@gmail.com>
2017-09-11 16:02:07 -0700

    dif

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907231858) do
+ActiveRecord::Schema.define(version: 20170911171514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20170907231858) do
     t.string "size", null: false
     t.text "description", null: false
     t.text "photos", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.integer "event_id"
+    t.string "type_reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
