5d5fba5c76f1d13985be850c4f0ed9e27e62fd30
Mark Kim <mark.jn.kim@gmail.com>
2017-09-11 20:14:08 -0700

    added event past scope, error partial, review form partial

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   belongs_to :owner, class_name: :User, optional: true
   has_many :reviews
 
+  scope :closed, -> { where( 'DateTime.now > ?', :end_date_time ) }
+
 end
