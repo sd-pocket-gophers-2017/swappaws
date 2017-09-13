208de63c8d4adcc8c443c53d5071e5ac238086ea
Mark Kim <mark.jn.kim@gmail.com>
2017-09-12 20:53:39 -0700

    materialize views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908052604) do
+ActiveRecord::Schema.define(version: 20170913002057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,17 +19,11 @@ ActiveRecord::Schema.define(version: 20170908052604) do
     t.string "location", null: false
     t.datetime "start_date_time", null: false
     t.datetime "end_date_time", null: false
-    t.integer "user_id", null: false
+    t.integer "sitter_id", null: false
+    t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "participants", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "role_id"
-    t.integer "event_id"
-    t.integer "user_id"
+    t.boolean "pending", default: true
   end
 
   create_table "pets", force: :cascade do |t|
@@ -39,15 +33,30 @@ ActiveRecord::Schema.define(version: 20170908052604) do
     t.string "breed", null: false
     t.string "size", null: false
     t.text "description", null: false
-    t.text "photos", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
+  create_table "requests", force: :cascade do |t|
+    t.text "note"
+    t.integer "owner_id"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "approved"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.integer "event_id"
+    t.string "type_reviews"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,6 +74,11 @@ ActiveRecord::Schema.define(version: 20170908052604) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "tokens", default: 3
+    t.string "profile_photo_file_name"
+    t.string "profile_photo_content_type"
+    t.integer "profile_photo_file_size"
+    t.datetime "profile_photo_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
