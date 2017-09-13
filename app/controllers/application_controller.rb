90d8a55e20da81be6176b3d6892797c698b17433
Carter McGee <carterm912@gmail.com>
2017-09-13 14:06:20 -0700

    photos works now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :state, :city, :street_address])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :state, :city, :street_address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :state, :city, :street_address, :profile_photo])
   end
 
 end
