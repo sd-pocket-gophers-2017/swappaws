aac28e64b391fcfca63ebd2f4aa4807c5049c616
Carter McGee <carterm912@gmail.com>
2017-09-13 14:50:02 -0700

    default photo works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :state, :city, :street_address])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :state, :city, :street_address, :profile_photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :state, :city, :street_address, :profile_photo, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :state, :city, :street_address, :profile_photo, :bio])
   end
 
 end
