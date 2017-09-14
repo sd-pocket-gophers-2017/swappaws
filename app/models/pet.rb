c391d4f7eba0a311a989a45a3af987a1263ddeb5
Carter McGee <carterm912@gmail.com>
2017-09-14 10:50:19 -0700

    add carousel functionality

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,7 +3,8 @@ class Pet < ApplicationRecord
   has_attached_file :photo, styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x300>'
+    medium: '300x300>',
+    custom: '800x400>'
   }
 
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
