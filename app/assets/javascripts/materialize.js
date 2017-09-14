8b39c115f60f6ca16a14d1b250dff2df108af05a
Mark Kim <mark.jn.kim@gmail.com>
2017-09-14 14:33:09 -0700

    fixed profile page, event cards

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,7 +4,7 @@ class Pet < ApplicationRecord
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>',
-    custom: '800x400>'
+    custom: '800x400#'
   }
 
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
