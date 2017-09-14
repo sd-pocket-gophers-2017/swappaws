10c944ac5186586bc4aa87300dc16d74418f0151
Mark Kim <mark.jn.kim@gmail.com>
2017-09-13 17:03:15 -0700

    fixed merge conflicts in events and pets for materialize

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170913002057) do
-
+ActiveRecord::Schema.define(version: 20170913210656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,9 +88,7 @@ ActiveRecord::Schema.define(version: 20170913002057) do
     t.string "state", null: false
     t.float "latitude"
     t.float "longitude"
-
     t.text "bio"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
