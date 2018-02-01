f5a666e264f3a189995a82cf68017499a34d1c3f
Blake Meyer <blake.ynwa@gmail.com>
2018-02-01 11:10:20 -0800

    Start implementing selenium with capybara

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "User creation" do 
+    it "allows for straightforward, successful creation of user accounts", :js => true do 
+      visit new_user_registration_path
+      fill_in 'First Name', :with => 'Antonio'
+      fill_in 'Last Name', :with => 'Margheriti'
+      fill_in 'Tell us a bit about yourself', :with => "I'm an Italian filmmaker"
+      fill_in 'Street address', :with => "518 Corner Place"
+      fill_in 'City', :with => "El Cajon"
+      fill_in 'State', :with => "California"
+      fill_in 'Email', :with => "amargheriti@gmail.com"
+      fill_in 'Password', :with => "password"
+      fill_in 'Password confirmation', :with => "password"
+      click_on 'Sign Up'
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
 end
