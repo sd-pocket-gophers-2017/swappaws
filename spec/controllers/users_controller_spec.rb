eb331272439f1e29023a21115d760eda87b8805e
Blake Meyer <blake.ynwa@gmail.com>
2018-02-01 17:11:00 -0800

    User account creation passes after implementing selenium and capybara

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,17 +19,17 @@ RSpec.describe UsersController, type: :controller do
   describe "User creation" do 
     it "allows for straightforward, successful creation of user accounts", :js => true do 
       visit new_user_registration_path
-      fill_in 'First Name', :with => 'Antonio'
-      fill_in 'Last Name', :with => 'Margheriti'
-      fill_in 'Tell us a bit about yourself', :with => "I'm an Italian filmmaker"
-      fill_in 'Street address', :with => "518 Corner Place"
-      fill_in 'City', :with => "El Cajon"
-      fill_in 'State', :with => "California"
-      fill_in 'Email', :with => "amargheriti@gmail.com"
-      fill_in 'Password', :with => "password"
-      fill_in 'Password confirmation', :with => "password"
+      fill_in 'user[first_name]', :with => 'Antonio'
+      fill_in 'user_last_name', :with => 'Margheriti'
+      fill_in 'user[bio]', :with => "I'm an Italian filmmaker"
+      fill_in 'user_street_address', :with => "518 Corner Place"
+      fill_in 'user_city', :with => "El Cajon"
+      fill_in 'user_state', :with => "California"
+      fill_in 'user_email', :with => "amargheriti@gmail.com"
+      fill_in 'user_password', :with => "password"
+      fill_in 'user_password_confirmation', :with => "password"
       click_on 'Sign Up'
-      expect(response).to have_http_status(:redirect)
+      expect(response).to have_http_status(:success)
     end
   end
 
