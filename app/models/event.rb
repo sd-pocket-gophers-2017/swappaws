273b79704db072dd75040f45ffc5aff98f97864d
trumpocalyspe <mark.jn.kim@gmail.com>
2018-02-10 00:00:03 -0800

    Validates Dates on Search for need a sitter pg

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,21 @@ class Event < ApplicationRecord
   # validates :distance, numericality: {only_integer: true}
   geocoded_by :full_street_address
   after_validation :geocode
+  validates_presence_of :start_date, :end_date
 
+  validate :end_date_is_after_start_date
+
+  ########
+  private
+  ########
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before start date")
+    end
+  end
+  
   def full_street_address
     "#{street_address}, #{city}, #{state}"
   end
